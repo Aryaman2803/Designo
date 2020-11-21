942aefdc1b7f02c569813bf080219abf42adcac1
Idriss Kone <idriss.kone@icloud.com>
2020-11-20 20:26:40 -0500

    styled first div of home project section

--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,33 @@ export default function Home() {
         <div className="Home-img-wrap">
           <img src={phoneImage} alt="" />
         </div>
+        <svg width="640" height="639" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient x1="0%" y1="50%" x2="100%" y2="50%" id="a">
+              <stop stop-color="#5D0202" stop-opacity="0" offset="0%" />
+              <stop stop-color="#5D0202" stop-opacity=".498" offset="100%" />
+            </linearGradient>
+          </defs>
+          <circle
+            fill="url(#a)"
+            transform="matrix(0 -1 -1 0 640 640)"
+            cx="320"
+            cy="320"
+            r="320"
+            fill-rule="evenodd"
+            opacity=".309"
+          />
+        </svg>
+      </section>
+      <section className="Home-projects">
+        <div className="project">
+          <div className="transparent-dark">
+            <h3>Web Design</h3>
+            <p>
+              View Projects<i class="fas fa-chevron-right"></i>
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   );
