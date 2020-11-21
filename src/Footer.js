302e9951adcf9fc1c202501a666164a45c6500c2
Idriss Kone <idriss.kone@icloud.com>
2020-11-21 13:32:24 -0500

    footer is styled

--- src/Footer.js
+++ src/Footer.js
@@ -1,33 +1,17 @@
 import React from "react";
 import logoLight from "./starter/assets/shared/desktop/logo-light.png";
+import facebook from "./starter/assets/shared/desktop/icon-facebook.svg";
+import instagram from "./starter/assets/shared/desktop/icon-instagram.svg";
+import pinterest from "./starter/assets/shared/desktop/icon-pinterest.svg";
+import twitter from "./starter/assets/shared/desktop/icon-twitter.svg";
+import youtube from "./starter/assets/shared/desktop/icon-youtube.svg";
+import circles from "./starter/assets/shared/mobile/bg-pattern-design-pages-intro-mobile.svg";
 import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="Footer">
       <div className="Footer-contact">
-        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-          <defs>
-            <linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%">
-              <stop offset="0%" stop-color="#5D0202" stop-opacity="0" />
-              <stop offset="100%" stop-color="#5D0202" stop-opacity=".498" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#a)" fill-rule="evenodd" opacity=".09">
-            <circle
-              cx="146"
-              cy="438"
-              r="146"
-              transform="matrix(1 0 0 -1 0 876)"
-            />
-            <circle
-              cx="146"
-              cy="146"
-              r="146"
-              transform="matrix(-1 0 0 1 292 0)"
-            />
-            <circle cx="438" cy="438" r="146" transform="matrix(0 1 1 0 0 0)" />
-          </g>
-        </svg>
+        <img src={circles} alt="circles" />
         <h2>Let's talk about your project</h2>
         <p>
           Ready to take it to the next level? Contact us today and find out how
@@ -44,14 +28,21 @@ export default function Footer() {
           <li>Contact</li>
         </ul>
         <p>
-          <span className="office">Designo Central Office</span>
+          <span className="office">Designo Central Office</span> <br />
           3886 Wellington Street <br />
           Toronto, Ontario M9C 3J5
         </p>
         <p>
-          <span className="office">Contact Us (Central Office)</span>
+          <span className="office">Contact Us (Central Office)</span> <br />
           P : +1 253-863-8967 <br />M : contact@designo.co
         </p>
+        <div className="icons-container">
+          <img src={facebook} alt="" />
+          <img src={youtube} alt="" />
+          <img src={twitter} alt="" />
+          <img src={pinterest} alt="" />
+          <img src={instagram} alt="" />
+        </div>
       </div>
     </div>
   );
