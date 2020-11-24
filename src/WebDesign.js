3f90d6e71d34283367eb2f3ab3fd48eb9cecb2cb
Idriss Kone <idriss.kone@icloud.com>
2020-11-24 16:13:06 -0500

    styled hero section of web design page

--- src/WebDesign.js
+++ src/WebDesign.js
@@ -1,5 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
+import headerCircle from "./starter/assets/home/desktop/bg-pattern-hero-home.svg";
+import "./WebDesign.scss";
 
 export default function WebDesign() {
   return (
@@ -11,23 +13,7 @@ export default function WebDesign() {
           We build websites that serve as powerful marketing tools and bring
           memorable brand experiences.
         </p>
-        <svg width="640" height="639" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <linearGradient x1="0%" y1="50%" x2="100%" y2="50%" id="a">
-              <stop stop-color="#5D0202" stop-opacity="0" offset="0%" />
-              <stop stop-color="#5D0202" stop-opacity=".498" offset="100%" />
-            </linearGradient>
-          </defs>
-          <circle
-            fill="url(#a)"
-            transform="matrix(0 -1 -1 0 640 640)"
-            cx="320"
-            cy="320"
-            r="320"
-            fill-rule="evenodd"
-            opacity=".309"
-          />
-        </svg>
+        <img src={headerCircle} alt="" />
       </section>
     </div>
   );
