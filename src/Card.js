5b0a440bd4550aecb0a36f01029440718b7298cd
Idriss Kone <idriss.kone@icloud.com>
2020-11-25 14:47:44 -0500

    web design page responsive on tablet

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,10 @@ import "./Card.scss";
 export default function Card(props) {
   return (
     <div className="Card">
-      <img src={props.image} alt="" />
+      <div className="">
+        <img src={props.image} alt="" />
+      </div>
+
       <div className="text-wrap">
         <h3>{props.title}</h3>
         <p> {props.text}</p>
