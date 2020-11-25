e9ea5ecd13178006917a7e8d0ddf642614d3ac4f
Idriss Kone <idriss.kone@icloud.com>
2020-11-25 17:22:10 -0500

    added graphic design page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Home from "./Home";
 import { Switch, Route } from "react-router-dom";
 import WebDesign from "./WebDesign";
 import AppDesign from "./AppDesign";
+import GraphicDesign from "./GraphicDesign";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/web-design" component={WebDesign} />
-        <Route exact path="/app-design" component={AppDesign} />
+        <Route exact path="/web-design" render={() => <WebDesign />} />
+        <Route exact path="/app-design" render={() => <AppDesign />} />
+        <Route exact path="/graphic-design" render={() => <GraphicDesign />} />
         <Route exact path="/" component={Home} />
       </Switch>
-      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/GraphicDesign.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Card from "./Card";
+import NavBar from "./NavBar";
+import { useMediaQuery } from "react-responsive";
+import headerCircle from "./starter/assets/home/desktop/bg-pattern-hero-home.svg";
+import multipleCircles from "./starter/assets/web-design/desktop/bg-pattern-intro-web.svg";
+import water from "./starter/assets/graphic-design/desktop/image-boxed-water.jpg";
+import change from "./starter/assets/graphic-design/desktop/image-change.jpg";
+import science from "./starter/assets/graphic-design/desktop/image-science.jpg";
+
+import "./WebDesign.scss";
+import Project from "./Project";
+import Footer from "./Footer";
+
+export default function WebDesign() {
+  const isTabletOrDesktop = useMediaQuery({
+    query: "(min-device-width:768px)",
+  });
+  return (
+    <div className="WebDesign">
+      <NavBar />
+      <section className="hero">
+        <div className="hero-text">
+          <h2> Graphic Design</h2>
+          <p>
+            We deliver eye-catching branding materials that are tailored to meet
+            your business objectives.
+          </p>
+        </div>
+
+        <img src={isTabletOrDesktop ? multipleCircles : headerCircle} alt="" />
+      </section>
+      <section className="projects">
+        <Card
+          image={change}
+          title="  Tim Brown
+"
+          text="A multi-  Solving   A book cover designed for Tim Brown’s new release, ‘Change’
+ "
+        />
+        <Card
+          image={water}
+          title="  Boxed water
+"
+          text="Site for   A simple packaging concept made for Boxed Water
+-  Product that lets you edit your favorite photos and videos at any time
+"
+        />
+        <Card
+          image={science}
+          title="Science!"
+          text="Site for low-  Get to   A poster made in collaboration with the Federal Art Project
+ your favorite internet superstar with the faceit app
+ "
+        />
+      </section>
+      <section className="other-projects">
+        <Project name="Web Design" id="web-design" />
+        <Project name="App Design" id="app-design" />
+      </section>
+      <Footer />
+    </div>
+  );
+}
