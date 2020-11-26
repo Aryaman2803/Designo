3c55a23096d9d60cad53ae06686904af8283fcd2
Idriss Kone <idriss.kone@icloud.com>
2020-11-26 17:20:05 -0500

    location page completed on mobile

--- src/Locations.js
+++ src/Locations.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import heroSvg from "./starter/assets/about/mobile/bg-pattern-hero-about-mobile.svg";
 import "./Locations.scss";
+import Footer from "./Footer";
 
 export default function Locations() {
   return (
@@ -10,7 +11,7 @@ export default function Locations() {
       <section className="country">
         <div className="img-wrap"></div>
         <div className="text-wrap">
-          {/* <img src={heroSvg} alt="" /> */}
+          <img src={heroSvg} alt="" />
           <h2>Canada</h2>
           <ul>
             <li>Designo Central Office</li>
@@ -25,6 +26,43 @@ export default function Locations() {
           </ul>
         </div>
       </section>
+      <section className="country">
+        <div className="img-wrap"></div>
+        <div className="text-wrap">
+          <img src={heroSvg} alt="" />
+          <h2>Australia</h2>
+          <ul>
+            <li> Designo AU Office</li>
+            <li>19 Balonne Street</li>
+            <li>New South Wales 2443</li>
+          </ul>
+          <br />
+          <ul>
+            <li> Contact</li>
+            <li>P : (02) 6720 9092</li>
+            <li>M : contact@designo.au</li>
+          </ul>
+        </div>
+      </section>
+      <section className="country" id="uk">
+        <div className="img-wrap"></div>
+        <div className="text-wrap">
+          <img src={heroSvg} alt="" />
+          <h2>United Kingdom</h2>
+          <ul>
+            <li>Designo UK Office</li>
+            <li>13 Colorado Way</li>
+            <li>Rhyd-y-fro SA8 9GA</li>
+          </ul>
+          <br />
+          <ul>
+            <li> Contact</li>
+            <li>P : 078 3115 1400</li>
+            <li>M : contact@designo.uk</li>
+          </ul>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 }
