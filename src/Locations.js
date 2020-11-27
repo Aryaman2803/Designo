d94cb381e4e8375e87b698a7ddb3cc7dbed8cb44
Idriss Kone <idriss.kone@icloud.com>
2020-11-27 17:00:15 -0500

    locations page made responsive for tablets

--- src/Locations.js
+++ src/Locations.js
@@ -13,17 +13,19 @@ export default function Locations() {
         <div className="text-wrap">
           <img src={heroSvg} alt="" />
           <h2>Canada</h2>
-          <ul>
-            <li>Designo Central Office</li>
-            <li>3886 Wellington Street</li>
-            <li>Toronto, Ontario M9C 3J5</li>
-          </ul>
-          <br />
-          <ul>
-            <li> Contact</li>
-            <li>P : +1 253-863-8967</li>
-            <li>M : contact@designo.co</li>
-          </ul>
+          <div className="list">
+            <ul>
+              <li>Designo Central Office</li>
+              <li>3886 Wellington Street</li>
+              <li>Toronto, Ontario M9C 3J5</li>
+            </ul>
+            <br />
+            <ul>
+              <li> Contact</li>
+              <li>P : +1 253-863-8967</li>
+              <li>M : contact@designo.co</li>
+            </ul>
+          </div>
         </div>
       </section>
       <section className="country">
@@ -31,17 +33,19 @@ export default function Locations() {
         <div className="text-wrap">
           <img src={heroSvg} alt="" />
           <h2>Australia</h2>
-          <ul>
-            <li> Designo AU Office</li>
-            <li>19 Balonne Street</li>
-            <li>New South Wales 2443</li>
-          </ul>
-          <br />
-          <ul>
-            <li> Contact</li>
-            <li>P : (02) 6720 9092</li>
-            <li>M : contact@designo.au</li>
-          </ul>
+          <div className="list">
+            <ul>
+              <li> Designo AU Office</li>
+              <li>19 Balonne Street</li>
+              <li>New South Wales 2443</li>
+            </ul>
+            <br />
+            <ul>
+              <li> Contact</li>
+              <li>P : (02) 6720 9092</li>
+              <li>M : contact@designo.au</li>
+            </ul>
+          </div>
         </div>
       </section>
       <section className="country" id="uk">
@@ -49,17 +53,19 @@ export default function Locations() {
         <div className="text-wrap">
           <img src={heroSvg} alt="" />
           <h2>United Kingdom</h2>
-          <ul>
-            <li>Designo UK Office</li>
-            <li>13 Colorado Way</li>
-            <li>Rhyd-y-fro SA8 9GA</li>
-          </ul>
-          <br />
-          <ul>
-            <li> Contact</li>
-            <li>P : 078 3115 1400</li>
-            <li>M : contact@designo.uk</li>
-          </ul>
+          <div className="list">
+            <ul>
+              <li>Designo UK Office</li>
+              <li>13 Colorado Way</li>
+              <li>Rhyd-y-fro SA8 9GA</li>
+            </ul>
+            <br />
+            <ul>
+              <li> Contact</li>
+              <li>P : 078 3115 1400</li>
+              <li>M : contact@designo.uk</li>
+            </ul>
+          </div>
         </div>
       </section>
       <Footer />
