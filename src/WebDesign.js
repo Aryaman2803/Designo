2fa130ee5698f24d84136f1b5d711587344e9976
Idriss Kone <idriss.kone@icloud.com>
2020-11-28 15:56:32 -0500

    deploy optimization

--- src/WebDesign.js
+++ src/WebDesign.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Card from "./Card";
 import NavBar from "./NavBar";
 import { useMediaQuery } from "react-responsive";
-import { Link } from "react-router-dom";
 import headerCircle from "./starter/assets/home/desktop/bg-pattern-hero-home.svg";
 import multipleCircles from "./starter/assets/web-design/desktop/bg-pattern-intro-web.svg";
 import express from "./starter/assets/web-design/desktop/image-express.jpg";
